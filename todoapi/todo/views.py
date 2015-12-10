b1c6efb85bf23b24f5afe90d819e57fa9ac2c1be
Bogdan Cornianu <bogdan.cornianu@3pillarglobal.com>
2015-12-10 14:50:37 +0200

    Fix indentation.

--- todoapi/todo/views.py
+++ todoapi/todo/views.py
@@ -16,23 +16,26 @@ class TodosView(APIView):
        serializer = TodoSerializer(todos, many=True)
        return Response(serializer.data)
 
-   def post(self, request):
+    def post(self, request):
        serializer = TodoSerializer(data=request.data)
        data = serializer.data
        user = User.objects.get(id=data['user'])
        todo = Todo(user=user, description=data["description"], completed=False)
        request.data["id"] = todo.id
        todo.save()
+       return Response(status=status.HTTP_201_CREATED)
 
-   def put(self, request):
+    def put(self, request, todo_id):
        serializer = TodoSerializer(data=request.data)
        data = serializer.data
        description = data["description"]
        completed = data["completed"]
-       todo = Todo(id=id, user=request.user, description=description, completed=completed)
+       todo = Todo(id=todo_id, user=request.user, description=description, completed=completed)
        todo.save()
+       return Response(status=status.HTTP_202_ACCEPTED)
 
-   def delete(self, request):
-       todo = Todo.objects.get(id=id)
+    def delete(self, request, todo_id):
+       todo = Todo.objects.get(id=todo_id)
        todo.delete()
+       return Response(status=status.HTTP_200_OK)
 
